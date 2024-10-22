72aeb0d4734a2b25dbb2756c9d5f14d4f8b23590
HakanTetik <fildisili88@gmail.com>
2024-10-22 21:17:03 +0200

    jenkins dosyasında değişiklik yapıldı

--- cypress.config.js
+++ cypress.config.js
@@ -1,34 +1,23 @@
 const { defineConfig } = require('cypress')
-const allureWriter = require('@shelex/cypress-allure-plugin/writer')
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // Allure reporter setup
-      allureWriter(on, config)
-      
-      // Existing task configuration
       on('task', {
         log(message) {
           console.log(`CYPRESS_LOG: ${message}`)
           return null
         },
       })
-      
-      return config
     },
-    // Existing configurations
-    baseUrl: 'https://www.franceculture.fr',
-    defaultCommandTimeout: 10000,
-    pageLoadTimeout: 30000,
-    responseTimeout: 30000,
-    requestTimeout: 10000,
     video: true,
     videosFolder: 'cypress/videos',
     screenshotOnRunFailure: true,
-    retries: {
-      runMode: 2,
-      openMode: 0
+    reporter: 'cypress-multi-reporters',
+    reporterOptions: {
+      configFile: 'reporter-config.json'
     }
-  }
+  },
+  defaultCommandTimeout: 60000,
+  pageLoadTimeout: 60000
 })
