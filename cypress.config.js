013f6d48a2c32b1fae07b6ae72ed57396a1963e1
HakanTetik <fildisili88@gmail.com>
2024-10-22 17:05:27 +0200

    jenkins dosyasında değişiklik yapıldı

--- cypress.config.js
+++ cypress.config.js
@@ -1,43 +1,12 @@
-const { defineConfig } = require('cypress')
-
-module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      on('task', {
-        log(message) {
-          console.log(`CYPRESS_LOG: ${message}`)
-          return null
-        },
-      })
-    },
-    video: true,
-    videoCompression: 32,
-    videosFolder: 'cypress/videos',
-    screenshotsFolder: 'cypress/screenshots',
-    reporter: 'cypress-multi-reporters',
-    reporterOptions: {
-      reporterEnabled: 'mochawesome, mocha-junit-reporter',
-      mochawesomeReporterOptions: {
-        reportDir: 'cypress/reports/mocha',
-        overwrite: false,
-        html: true,
-        json: true,
-        timestamp: true,
-        charts: true,
-        embeddedScreenshots: true,
-        inlineAssets: true,
-        saveAllAttempts: false,
-        code: true
-      },
-      mochaJunitReporterReporterOptions: {
-        mochaFile: 'cypress/reports/junit/results-[hash].xml'
-      }
-    }
+{
+  "reporterEnabled": "spec, mocha-junit-reporter, mochawesome",
+  "mochaJunitReporterReporterOptions": {
+      "mochaFile": "cypress/reports/junit/results-[hash].xml"
   },
-  defaultCommandTimeout: 60000,
-  pageLoadTimeout: 60000,
-  retries: {
-    runMode: 2,
-    openMode: 0
+  "mochawesomeReporterOptions": {
+      "reportDir": "cypress/reports/json",
+      "overwrite": false,
+      "html": false,
+      "json": true
   }
-})
+}
